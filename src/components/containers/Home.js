5f45d320c6db8315b8534380142e723abb00b183
Miguel Pardo <migsub77@gmail.com>
2017-07-04 21:30:07 -0700

    added social icons and mixins

--- src/components/containers/Home.js
+++ src/components/containers/Home.js
@@ -1,10 +1,14 @@
-import React from 'react';
 import '../../_styles/components/Home/Home.css';
+import React from 'react';
+
+import SocialItems from '../global/SocialItems.js';
+
 
 export default function Home() {
   return(
     <div id="Home">
       <h1>This is the Home Page</h1>
+      <SocialItems />
     </div>
   )
 }
